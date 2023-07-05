4d8aee0d814e3018e2c7f783230ae829a61b8ba1
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 19:05:45 -0400

    commented additional paths

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name= 'index'),
+    #path('', views.index, name= 'index'),
     path('user_registeration/', views.user_registeration, name='user_registeration'),
-    path('employer_regisetration/', views.employer_registration, name='employer_registeration'),
-    path('job_creation/', views.job_creation, name='job_creation')
+    #path('employer_registeration/', views.employer_registration, name='employer_registeration'),
+    #path('job_creation/', views.job_creation, name='job_creation')
 ]
