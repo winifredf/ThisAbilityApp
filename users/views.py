354f2df017814fc36ec230d30a9b8251211a97d0
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 10:03:02 -0400

    created user_registration function

--- users/views.py
+++ users/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .models import User, Employer, Job
+from .forms import UserRegistrationForm, EmployerRegistrationForm, JobCreationForm
 
 # Create your views here.
+
+def user_registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.ceaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+            return redirect('dashboard')
+        else:
+            form = UserRegistrationForm()
+            return render(request, 'user_registration.html', {'form': form})
