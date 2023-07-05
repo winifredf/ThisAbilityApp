3909e98e1e4b6d62597e0a523f841fc2b9836ad9
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 01:20:16 -0400

    registeres models

--- users/admin.py
+++ users/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import User, Employer, Job
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Employer)
+admin.site.register(Job)
