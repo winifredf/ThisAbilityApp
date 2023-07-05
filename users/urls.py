099e0a4b6251d77f5ac218f1d6d17f913ebbac20
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 17:02:57 -0400

    added User url paths

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('user_registration/', views.user_registeration, name='user_registration'),
+    path('employer_registration/', views.employer_registration, name='employer_registration'),
+    path('job_creation/', views.job_creation, name='job_creation'),
+]
