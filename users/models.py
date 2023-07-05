c9dde92488403ccd3ecfb9cb02bd57e37eea6485
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 00:45:22 -0400

    created Employer and job models

--- users/models.py
+++ users/models.py
@@ -12,4 +12,22 @@ class User(models.Model):
     password = models.CharField(max_length=200)
     abilities = models.TextField()
 
+class Employer(models.Model):
+    name = models.CharField(max_length=100)
+    requirements = models.TextField()
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class job(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    salary = models.PositiveIntegerField()
+    location = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    
+
 
