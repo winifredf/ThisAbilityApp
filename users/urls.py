472524bbd70fb491a76cb59e38d20b84cfee6e89
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 17:07:22 -0400

    commented emp_reg, job_creat path for now.

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('user_registration/', views.user_registeration, name='user_registration'),
-    path('employer_registration/', views.employer_registration, name='employer_registration'),
-    path('job_creation/', views.job_creation, name='job_creation'),
+    #path('employer_registration/', views.employer_registration, name='employer_registration'),
+    #path('job_creation/', views.job_creation, name='job_creation'),
 ]
