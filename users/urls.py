1551f4bbfe8ff9cbd5c09146f7a0cf6c1f72963a
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 18:58:03 -0400

    deleted , ln8

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name= 'index'),
     path('user_registration/', views.user_registeration, name='user_registration'),
     path('employer_registration/', views.employer_registration, name='employer_registration'),
-    path('job_creation/', views.job_creation, name='job_creation'),
+    path('job_creation/', views.job_creation, name='job_creation')
 ]
