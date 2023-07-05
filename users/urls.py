2406e2f4d9da05938454c04925cc2812bf3820bc
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 19:04:07 -0400

    corrected spelling registeration ln7

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= 'index'),
-    path('user_registration/', views.user_registeration, name='user_registration'),
-    path('employer_registration/', views.employer_registration, name='employer_registration'),
+    path('user_registeration/', views.user_registeration, name='user_registeration'),
+    path('employer_regisetration/', views.employer_registration, name='employer_registeration'),
     path('job_creation/', views.job_creation, name='job_creation')
 ]
