b69ece7f1d1d6dcf98f6e17929438090d04a69bf
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 00:39:55 -0400

    created User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    address = models.CharField(max_length=200)
+    gender = models.CharField(max_length=10)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    abilities = models.TextField()
+
+
