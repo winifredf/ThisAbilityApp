8e47f3656f75bc58ff6f02e3806d11899f994a66
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-05 20:41:44 -0400

    added str method

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= 'index'),
-    path('user_registeration/', views.user_registeration, name='user_registeration'),
-    #path('employer_registeration/', views.employer_registration, name='employer_registeration'),
-    #path('job_creation/', views.job_creation, name='job_creation')
+    path('user_registration/', views.user_registration, name='user_registration'),
+    
 ]
