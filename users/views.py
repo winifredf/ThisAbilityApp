2b7555c9a6e2d7d50a0fd8c52bb683cf7305f8ef
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-06 00:35:16 -0400

    updated index function

--- users/views.py
+++ users/views.py
@@ -7,19 +7,19 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'This_Ability_App/index.html')
 
 def user_registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-            return redirect('dashboard')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'user_registration.html', {'form': form})
+    #if request.method == 'POST':
+        #form = UserRegistrationForm(request.POST)
+        #if form.is_valid():
+         #   form.save()
+           # username = form.cleaned_data['username']
+            #password = form.cleaned_data['password1']
+          #  #user = authenticate(request, username=username, password=password)
+            #if user is not None:
+                #login(request, user)
+            #return redirect('dashboard')
+    #else:
+        #form = UserRegistrationForm()
+    #return render(request, 'user_registration.html', {'form': form})
